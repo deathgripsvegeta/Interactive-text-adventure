bafeab6004271561793b55c1eff7d2175cd265a8
deathgripsvegeta <sophiahowell831@gmail.com>
2022-09-16 12:08:51 -0700

    scene chouces

--- interactive_text_project/Assets/Scripts/SceneLoader.cs
+++ interactive_text_project/Assets/Scripts/SceneLoader.cs
@@ -11,9 +11,20 @@ public class SceneLoader : MonoBehaviour
      SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
-    public void LoasStartScene()
+    public void LoadStartScene()
     {
         SceneManager.LoadScene(0);
+    
+    }
+    
+     public void HamperScene()
+    {
+        SceneManager.LoadScene(4);
+    
+    }
+     public void BalconyScene()
+    {
+        SceneManager.LoadScene(5);
     
     }
 
